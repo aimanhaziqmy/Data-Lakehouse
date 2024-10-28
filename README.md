dd0c885c73e01de6282cfeebb9bffb6715e92e89
aimanhaziqmy <aimanhaziqyazik@gmail.com>
2024-10-28 20:59:29 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Todo 
+
+Consume with apache spark for streaming
+Consume with apache flink for batch processing
---
+++ minio_config.py
@@ -0,0 +1,5 @@
+config = {
+    'access_key' : 'qETkL1qm0mUhvHgE9ADJ',
+    'secret_key' : 'v98rN4XyA1QEt0SknCrYBfkbXO1i9yFEt22EEGzE',
+
+}
